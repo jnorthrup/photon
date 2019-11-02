51efae5421667bd707edcbf2cc22df4e3fff205c
Jim Northrup <jim@vsiwest.com>
2019-11-02 18:21:02 +0700

    combinator only

--- lownal/src/main/java/com/fnreport/nards/Terms.kt
+++ lownal/src/main/java/com/fnreport/nards/Terms.kt
@@ -1,9 +1,25 @@
 package com.fnreport.nards
 
 
+import com.fnreport.nards.accounting.desire
+import com.fnreport.nards.accounting.truth
+import com.fnreport.nards.statement.operation_to_be_exdecuted
+import com.fnreport.nards.statement.related_pair_of_terms
+import com.fnreport.nards.statement.statement_name
+import com.fnreport.nards.term.comma
+import com.fnreport.nards.term.cparen
+import com.fnreport.nards.term.oparen
+import com.fnreport.nards.term_connector.*
+import com.fnreport.nards.term_set.extensional_set
+import com.fnreport.nards.term_set.intensional_set
 import com.github.h0tk3y.betterParse.combinators.*
-import com.github.h0tk3y.betterParse.lexer.*
+import com.github.h0tk3y.betterParse.grammar.Grammar
+import com.github.h0tk3y.betterParse.lexer.Token
+import com.github.h0tk3y.betterParse.lexer.TokenMatch
+import com.github.h0tk3y.betterParse.lexer.literalToken
+import com.github.h0tk3y.betterParse.lexer.regexToken
 import com.github.h0tk3y.betterParse.parser.Parser
+import com.github.h0tk3y.betterParse.utils.Tuple
 
 open class Omega
 
@@ -12,39 +28,14 @@ object nd_ : Omega()
 val `Ω` = nd_
 
 
-val period        by lazy { lit('.') }
-val ws        by lazy { regexToken("\\s*".toRegex(),false) }
-val  capture by lazy { regexToken(".*".toRegex(),false) }
+val period by lazy { lit('.') }
+val ws by lazy { regexToken("\\s*".toRegex(), false) }
+val capture by lazy { regexToken(".*".toRegex(), false) }
 //ws("\\s*"),
 //
-fun  lit(i: Any)  = literalToken(i.toString(), i.toString() as String, false)
+fun lit(i: Any) = literalToken(i.toString(), i.toString(), false)
 
-inline infix fun <reified T, P : Parser<T>> Any.`&`(that: Omega) = lit(this) as P
-inline infix fun <reified T, P : Parser<T>> Char.`&`(that: Omega) = CharToken(this.toString(), this) as P
-inline infix fun <reified T, P : Parser<T>> Char.`&`(that: P) = (CharToken(this.toString(), this) `&` that) as P
-inline infix fun <reified T, P : Parser<T>> Char.`?`(that: P) = (CharToken(this.toString(), this) `?` that) as P
-inline infix fun <reified T, P : Parser<T>> Char.`|`(that: P) = (CharToken(this.toString(), this) `|` that) as P
-inline infix fun <reified T, P : Parser<T>> P.`&`(other: P) = (this and other) as P
-inline infix fun <reified T, P : Parser<T>> P.`&`(that: Char) = this `&` CharToken(that.toString(), that) as P
-inline infix fun <reified T, P : Parser<T>> P.`&`(that: String) = this `&` lit(that) as P
-inline infix fun <reified T, P : Parser<T>> P.`?`(c: Char) = ((this `?` `Ω`) `&` c)
-inline infix fun <reified T, P : Parser<T>> P.`?`(c: String) = ((this `?` `Ω`) `&` lit(c)) as P
-inline infix fun <reified T, P : Parser<T>> P.`?`(`Ω`: Omega) = optional(this) as P
-inline infix fun <reified T, P : Parser<T>> P.`?`(that: P) = (optional(this) `&` that) as P
-inline infix fun <reified T, P : Parser<T>> P.`|`(that: P) = OrCombinator(listOf(this, that)) as P
-inline infix fun <reified T, P : Parser<T>> P.`|`(that: String) = OrCombinator(listOf(this, lit(that) as P)) as P
-inline infix fun <reified T, P : Parser<T>> Regex.`&`(that: Omega): P = (RegexToken(this.toString(), this.toString())) as P
-inline infix fun <reified T, P : Parser<T>> String.`&`(that: P) = (lit(this) `&` that) as P
-inline infix fun <reified T, P : Parser<T>> String.`?`(that: P) = (lit(this) `?` that) as P
-inline infix fun <reified T, P : Parser<T>> String.`|`(that: P) = (lit(this) `|` that) as P
-inline infix operator fun <reified T, P : Parser<T>> P.plus(`Ω`: Omega) = oneOrMore(this) as P
-inline infix operator fun <reified T, P : Parser<T>> P.plus(that: P) = (oneOrMore(this) `&` that) as P
-inline infix operator fun <reified T, P : Parser<T>> P.plus(that: String) = (oneOrMore(this) `&` that) as P
-inline infix operator fun <reified T, P : Parser<T>> P.times(`Ω`: Omega) = zeroOrMore(this) as P
-inline infix operator fun <reified T, P : Parser<T>> P.times(that: P) = (zeroOrMore(this as P) `&` that) as P
-inline infix operator fun <reified T, P : Parser<T>> P.times(that: String) = (zeroOrMore(this) `&` lit(that)) as P
-inline infix operator fun <reified T, P : Parser<T>> String.plus(that: P) = oneOrMore(lit(this)) `&` that
-inline infix operator fun <reified T, P : Parser<T>> String.times(that: P) = zeroOrMore(lit(this)) `&` that
+
 /*
 task ::= [budget] sentence                       (* task to be processed *)
 
@@ -126,30 +117,141 @@ confidence : #"[0]?\.[0]*[1-9]{1}[0-9]*"             (* 0 <  x <  1 *)
 */
 
 
-enum class accounting(p: Parser<TokenMatch>) : Parser<TokenMatch> by p {
+/**
+compound-term ::= op-ext-set term {"," term} "}"          (* extensional set *)
+| op-int-set term {"," term} "]"          (* intensional set *)
+| "("op-multi"," term {"," term} ")"      (* with prefix operator *)
+| "("op-single"," term "," term ")"       (* with prefix operator *)
+| "(" term {op-multi term} ")"            (* with infix operator *)
+| "(" term op-single term ")"             (* with infix operator *)
+| "(" term {","term} ")"                  (* product, new notation *)
+| "(" op-ext-image "," term {"," term} ")"(* special case, extensional image *)
+| "(" op-int-image "," term {"," term} ")"(* special case, \ intensional image *)
+| "(" op-negation "," term ")"            (* negation *)
+| op-negation term                        (* negation, new notation *)
+ */
+
+
+enum class op_multi(s: String, symbol: String) : Parser<TokenMatch> by literalToken(symbol, s, false) {
+    conjunction("&&", "∧"),
+    /**product*/
+    product("*", "×"),
+    /**disjunction*/
+    disjunction("||", "∨"),
+    /**parallel events*/
+    parallel_events("&|", ";"),
+    /**sequential events*/
+    sequential_events("&/", ","),
+    /**intensional intersection*/
+    intensional_intersection("|", "∪"),
+    /**extensional intersection*/
+    extensional_intersection("&", "∩"),
+    /**placeholder?*/
+    image("_", "◇")
+}
+
+enum class term_set(pair: Pair<String, String>) {
+    intensional_set("[" to "]"),
+    extensional_set("{" to "}"), ;
+
+    val first = lit(pair.first)
+    val second = lit(pair.second)
+}
+
+enum class term_connector(s: Any?, symbol: String? = null, val lit: Token = literalToken(symbol.takeIf { it != null }
+        ?: Enum<*>::name as String, s.toString(), false)) : Parser<TokenMatch> by lit {
+    negation("--", "¬"),
+    intensional_image('\\'),
+    extensional_image('/')
+}
+
+
+/**op-single*/
+enum class op_single(s: String, symbol: String) : Parser<TokenMatch> by literalToken(symbol, s, false) {
+
+    /**`extensional difference`*/
+    extensional_difference("-", "−"),
+    /**`intensional difference`*/
+    intensional_difference("~", "⦵"),
+}
+
+
+enum class tense(s: String, symbol: String) : Parser<TokenMatch> by literalToken(symbol, s, false) {
+    /** future event */
+    future_event(":/:", "/⇒"),
+    /** present event */
+    present_event(":|:", "|⇒"),
+    /** :\: past event */
+    past_event(":\\:", "\\⇒")
+    ;
+}
+
+object term : Grammar<Any?>() {
+    /** conjunction */
+
+    /** conjunction */
+
+    val compoundedTerms by lazy { separatedTerms(term, comma, false) }
+
+    val comma = lit(",")
+
+    val oparen = lit("(")
+    val cparen = lit(")")
+    val ext_set = -extensional_set.first * compoundedTerms * -extensional_set.second
+    val int_set = -intensional_set.first * compoundedTerms * -intensional_set.second
+    val prefixed_mutli_value = -oparen * (OrCombinator(op_multi.values().toList())) * -comma * compoundedTerms * -cparen
+    val prefixed_single_value = -oparen * (/*OrCombinator(op_multi.values().toList()) or*/ OrCombinator(op_single.values().toList())) * -comma * 2.times(-comma * term) * -cparen
+    val infix_multi_value = -oparen * term * (oneOrMore(OrCombinator(op_multi.values().toList())) and term) * -cparen
+    val infix_single_value = -oparen * term * OrCombinator(op_single.values().toList()) * term * -cparen
+    val special_extensional_image_case = -oparen * term * compoundedTerms * -cparen
+    val special_case_intentionsal_image = -oparen * (intensional_image or extensional_image) * -comma * compoundedTerms * -cparen
+    val negation1 = -oparen * negation * -comma * term * -cparen
+    val negation2 = negation * term
+    val compound_term = ext_set or
+            int_set or
+            prefixed_mutli_value or
+            prefixed_single_value or
+            infix_multi_value or
+            infix_single_value or
+            special_extensional_image_case or
+            special_case_intentionsal_image or
+            negation1 or
+            negation2
+    /**
+    term ::= word                                    (* an atomic constant term *)
+    | variable                                (* an atomic variable term *)
+    | compound-term                           (* a term with internal structure *)
+    | statement                               (* a statement can serve as a term *)
+     */
+    /**
+    term ::= word                                    (* an atomic constant term *)
+    | variable                                (* an atomic variable term *)
+    | compound-term                           (* a term with internal structure *)
+    | statement                               (* a statement can serve as a term *)
+     */
+
+    override val rootParser: Parser<Any?> by lazy { word or OrCombinator(variable.values().toList()) or compound_term or (related_pair_of_terms or statement_name or operation_to_be_exdecuted) }
+
+}
+
+object accounting : Grammar<Tuple>() {
     /** same format, different interpretations */
-    desire('%' `&` fragment.frequency `&` ';' `&` fragment.confidence `?` '%'),
+    val desire = -lit('%') and fragment.frequency and optional(-lit(';') and fragment.confidence) and -lit('%')
     /** two numbers in [0,1]x(0,1) */
-    truth('%' `&` fragment.frequency `&` ';' `&` fragment.confidence `?` '%'),
+    val truth = -lit('%') and fragment.frequency and optional(-lit(';') and fragment.confidence) and -lit('%')
     /** three numbers in [0,1]x(0,1)x[0,1] */
-    budget('$' `&` fragment.priority `&` ';' `&` fragment.durability `?` ';' `&` fragment.quality `?` '$');
+    val budget = -lit('$') and fragment.priority and optional(-lit(';') and fragment.durability) and optional(-lit(';') and fragment.quality) and -lit('$')
+    override val rootParser =desire or truth or budget
+
 }
 
-
-enum class variable(parser: Parser<TokenMatch>) : Parser<TokenMatch> by parser {
-    /** independent variable */
-    independent_variable(lit('$') `&` fragment.word),
-    /** dependent variable */
-    dependent_variable(lit('#') `&` fragment.word),
-    /** query variable in question */
-    query_variable_in_question(lit('?') `&` fragment.word),
-}
 
 val fractionalpart = "([01]([.]\\d*)?|[.]\\d{1,})"
+/** unicode string */
+val word = regexToken("[^\\s]+") map TokenMatch::text
 
-enum class fragment(s: String) : Parser<TokenMatch> by regexToken(  s.toRegex(), false) {
-    /** unicode string */
-    word("[^\\s]+"),
+enum class fragment(s: String) : Parser<Double> by regexToken(s) map { tokenMatch -> tokenMatch.text.toDouble() } {//(s: String) : Parser<TokenMatch> by regexToken(s.toRegex(), false) {
+
     /** 0 <= x <= 1 */
     priority(fractionalpart),
     /** 0 <  x <  1 */
@@ -193,58 +295,44 @@ enum class copula(s: String, symbol: String) : Parser<TokenMatch> by literalToke
 
 }
 
-enum class term_set(op: String, cl: String) {
-    intensional_set("[", "]"),
-    extensional_set("{", "}"), ;
-}
 
-enum class term_connector(s: Any?, symbol: String? = null, val lit: Token = literalToken(symbol.takeIf { it != null }
-        ?: Enum<*>::name as String, s.toString(), false)) : Parser<TokenMatch> by lit {
-    negation("--", "¬"),
-    intensional_image('\\'),
-    extensional_image('/')
-}
-
-
-/** conjunction */
-enum class op_multi(s: String, symbol: String) : Parser<TokenMatch> by literalToken(symbol, s, false) {
-    conjunction("&&", "∧"),
-    /**product*/
-    product("*", "×"),
-    /**disjunction*/
-    disjunction("||", "∨"),
-    /**parallel events*/
-    parallel_events("&|", ";"),
-    /**sequential events*/
-    sequential_events("&/", ","),
-    /**intensional intersection*/
-    intensional_intersection("|", "∪"),
-    /**extensional intersection*/
-    extensional_intersection("&", "∩"),
-    /**placeholder?*/
-    image("_", "◇")
-}
-
-/**op-single*/
-enum class op_single(s: String, symbol: String) : Parser<TokenMatch> by literalToken(symbol, s, false) {
-
-    /**`extensional difference`*/
-    extensional_difference("-", "−"),
-    /**`intensional difference`*/
-    intensional_difference("~", "⦵"),
+/**
+variable ::= "$"word                                 (* independent variable *)
+|            "#"word                                 (* dependent variable *)
+|            "?"word                                 (* query variable in question *)
+ */
+enum class variable(s: String, val x: AndCombinator<String> = -lit(s) * word) : Parser<String> by x {
+    independent_variable("$"),
+    dependent_variable("#"),
+    query_variable("?"),
 }
 
 
-enum class tense(s: String, symbol: String) : Parser<TokenMatch> by literalToken(symbol, s, false) {
-    /** future event */
-    future_event(":/:", "/⇒"),
-    /** present event */
-    present_event(":|:", "|⇒"),
-    /** :\: past event */
-    past_event(":\\:", "\\⇒")
-    ;
-}
+/*
+(* an atomic constant term *)
+| variable                                (* an atomic variable term *)
+| compound-term                           (* a term with internal structure *)
+| statement                               (* a statement can serve as a term *)
+*/
 
+/**
+statement ::= <"<">term copula term<">">              (* two terms related to each other *)
+| <"(">term copula term<")">                          (* two terms related to each other, new notation *)
+| term                                                (* a term can name a statement *)
+| "(^"word {","term} ")"                              (* an operation to be executed *)
+| word"("term {","term} ")"                           (* an operation to be executed, new notation *)
+ */
+
+
+object statement : Grammar<Any?>() {
+    val related_pair_of_terms = (oparen * term * OrCombinator(copula.values().toList()) * term * cparen) or
+            (lit("<") * term * OrCombinator(copula.values().toList()) * term * lit(">"))
+    val statement_name = term
+    val operation_to_be_exdecuted = (-lit("(^") * word * -comma * term * -cparen ) or
+            (word * -oparen * term * -comma * term * -cparen)
+    override val rootParser by related_pair_of_terms or statement_name or operation_to_be_exdecuted
+
+}
 
 /**
 sentence ::= statement"." [tense] [truth]            (* judgement to be absorbed into beliefs *)
@@ -254,10 +342,10 @@ sentence ::= statement"." [tense] [truth]            (* judgement to be absorbed
  */
 
 
-enum class sentence(s: Parser<TokenMatch>, symbol: String? = null) : Parser<TokenMatch> by s {
-    judgement(capture `&` '.' `&` OrCombinator(tense.values().asList()) `?` accounting.truth `?` `Ω`),
-    valuation(capture `&` '?' `&` OrCombinator(tense.values().asList()) `?` accounting.truth `?` `Ω`),
-    goal(capture `&` '!' `&` (accounting.desire) `?` `Ω`),
-    interest(capture `&` '@' `&` (accounting.desire) `?` `Ω`, "¿"),
-    ;
+object sentence : Grammar<Any?>() {
+    val judgement = (capture * -lit('.')  * optional( OrCombinator(tense.values().asList())) * optional(  truth)  )
+    val valuation = capture * -lit('?') * optional(OrCombinator(tense.values().asList())) *optional(  truth )
+    val goal = (capture  * -lit('!') * optional(  desire )  )
+    val interest = (capture * -lit('@') * optional(desire)) //       "¿")
+    override val rootParser= judgement or valuation or goal or interest
 }
